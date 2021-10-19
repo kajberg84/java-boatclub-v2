09e775e5ad28bed0750b706e656319a9c01f347b
Kaj Berg <kb223aw@student.lnu.se>
2021-10-19 09:10:11 +0200

    fixed style issues

--- src/main/java/model/rules/Soft17HitStrategy.java
+++ src/main/java/model/rules/Soft17HitStrategy.java
@@ -1,8 +1,8 @@
 package model.rules;
 
 import model.Card;
-import model.Player;
 import model.Card.Value;
+import model.Player;
 
 class Soft17HitStrategy implements HitStrategy {
   private static final int hitLimit = 17;
@@ -15,13 +15,11 @@ class Soft17HitStrategy implements HitStrategy {
   }
 
   private boolean hasAce(Iterable<Card> hand) {
-   for (Card card : hand) {
-    if (card.getValue() == Value.Ace) {
-      return true;
+    for (Card card : hand) {
+      if (card.getValue() == Value.Ace) {
+        return true;
+      }
     }
-   }
-   return false;
+    return false;
   }
 }
-
-// If hand is 17 and one card is an Ace --> dealer can take another card and ace is valued as 1
