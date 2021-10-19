08b970b15b2de908e20616ad590471f390ac4a90
Kaj Berg <kb223aw@student.lnu.se>
2021-10-19 10:34:54 +0200

    Added testcases for soft17

--- src/test/java/model/BlackJackTest.java
+++ src/test/java/model/BlackJackTest.java
@@ -16,12 +16,51 @@ public class BlackJackTest {
 
     Card.Mutable c1 = new Card.Mutable(Card.Color.Hearts, Card.Value.Ace);
     Card.Mutable c2 = new Card.Mutable(Card.Color.Hearts, Card.Value.Six);
+
+    c1.show(true);
     dealer.dealCard(c1);
+    c2.show(true);
     dealer.dealCard(c2);
-    
+
     HitStrategy hitRule = rulesFactory.getHitRule();
 
     assertEquals(true, hitRule.doHit(dealer),"Dealer should take another card");
+  }
+
+  @Test
+  public void dealerTest_2() {
+    DummyRulesFactory rulesFactory = new DummyRulesFactory();
+    DummyDealer dealer = new DummyDealer(rulesFactory);
+
+    Card.Mutable c1 = new Card.Mutable(Card.Color.Spades, Card.Value.Ten);
+    Card.Mutable c2 = new Card.Mutable(Card.Color.Clubs, Card.Value.Seven);
+
+    c1.show(true);
+    dealer.dealCard(c1);
+    c2.show(true);
+    dealer.dealCard(c2);
+
+    HitStrategy hitRule = rulesFactory.getHitRule();
+    assertEquals(false, hitRule.doHit(dealer), "Dealer should not take another card");
+  }
+
+  @Test
+  public void dealerTest_3() {
+    DummyRulesFactory rulesFactory = new DummyRulesFactory();
+    DummyDealer dealer = new DummyDealer(rulesFactory);
+
+    Card.Mutable c1 = new Card.Mutable(Card.Color.Hearts, Card.Value.Ace);
+    Card.Mutable c2 = new Card.Mutable(Card.Color.Hearts, Card.Value.Six);
+    Card.Mutable c3 = new Card.Mutable(Card.Color.Hearts, Card.Value.Ten);
+
+    c1.show(true);
+    dealer.dealCard(c1);
+    c2.show(true);
+    dealer.dealCard(c2);
+    c3.show(true);
+    dealer.dealCard(c3);
+
+    assertEquals(17, dealer.calcScore(), "Dealer should have a score of 17");
   }
 
   class DummyDealer extends Dealer {
