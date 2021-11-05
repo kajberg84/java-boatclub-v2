74bdbadfce4451d25c5923e333a8d2a56372850d
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-11-05 17:35:01 +0100

    Instantiate visitor

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -1,11 +1,14 @@
 package controller;
 
 import model.Game;
+import model.RulesVisitor;
 import model.rules.AmericanSoft17PlayerAdvantageFactory;
 import model.rules.RulesFactory;
 import view.BaseView;
+import view.EnglishRulesPrinterVisitor;
 import view.EnglishView;
 // import view.SwedishView;
+// import view.SwedishRulesPrinterVisitor;
 
 /**
  * Starts the application using the console.
@@ -20,8 +23,10 @@ public class App {
     RulesFactory r = new AmericanSoft17PlayerAdvantageFactory();
     Game g = new Game(r);
     BaseView v = new EnglishView(); // new SwedishView();
-    // View v = new SwedishView();
-    Player ctrl = new Player(v, g);
+    // BaseView v = new SwedishView();
+    RulesVisitor visitor = new EnglishRulesPrinterVisitor();
+    // RulesVisitor visitor = new SwedishRulesPrinterVisitor();
+    Player ctrl = new Player(v, g, visitor);
 
     v.displayWelcomeMessage();
     
